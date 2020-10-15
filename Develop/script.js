72f8a1069985425fe52b0f02f44f7e81b981e97e
Emily Blair <emilyblair96@gmail.com>
2020-10-15 17:42:10 -0500

    Trouble shooting 404 message cont.

--- Develop/script.js
+++ Develop/script.js
@@ -3,11 +3,11 @@ var generateBtn = document.querySelector("#generate");
 // Add event listener to generate button, adds this method to HTML element, calls the writePassword functio defined above
 generateBtn.addEventListener("click", writePassword);
 
-var options = ["special", "alphabetUpper", "alphabetLower", "numbers"]
-var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
-var alphabetLower = ["a", "b", "c"]
-var numbers = ["0", "1", "2"]
+// var options = ["special", "alphabetUpper", "alphabetLower", "numbers"]
+// var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
+// var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
+// var alphabetLower = ["a", "b", "c"]
+// var numbers = ["0", "1", "2"]
 
 // Write password to the #password input
 function writePassword() {
@@ -56,21 +56,62 @@ function generatePassword() {
 
     var specialCharacters = confirm("Would you like your password to include special characters?")
 
-    for (i=0; i<=passwordLength; i++) {
-    // alert("Let's make a password!")
-    var userPassword = [i]
-  
-  
+    var password = "";
+    var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-="
+    var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    var alphabetLower = "abcdefghijklmnopqrstuvwxyz"
+    var num = "0123456789"
+    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
+    var chars2 = special.concat(alphabetUpper, num)
+    var chars3 =special.concat(alphabetLower, num)
+    var chars4 = special.concat(alphabetUpper, alphabetLower)
+    var chars5 = alphabetUpper.concat(alphabetLower, num)
+    var char6 = num.concat(special)
+ 
     if (lowercase === true && uppercase == true && numbers === true && specialCharacters === true) {
-      userPassword.push(randomSelector())
-      console.log(userPassword)
+      for (i=1;i<passwordLength;i++) {
+        var c = Math.floor(Math.random()*chars.length + 1);
+        password += chars.charAt(c)
       }
+      return password;
+      }
+    else if (lowercase === false && uppercase == true && numbers === true && specialCharacters === true) {
+      for (i=1;i<passwordLength;i++) {
+        var c = Math.floor(Math.random()*chars2.length + 1);
+        password += chars2.charAt(c)
+      }
+      return password;
+      }
+    else if (lowercase === true && uppercase == false && numbers === true && specialCharacters === true) {
+        for (i=1;i<passwordLength;i++) {
+          var c = Math.floor(Math.random()*chars3.length + 1);
+          password += chars3.charAt(c)
+        }
+        return password;
+        }
+    else if (lowercase === true && uppercase == true && numbers === false && specialCharacters === true) {
+      for (i=1;i<passwordLength;i++) {
+        var c = Math.floor(Math.random()*chars4.length + 1);
+        password += chars4.charAt(c)
+      }
+      return password;
+      }
+    else if (lowercase === true && uppercase == true && numbers === true && specialCharacters === false) {
+      for (i=1;i<passwordLength;i++) {
+        var c = Math.floor(Math.random()*chars5.length + 1);
+        password += chars5.charAt(c)
+      }
+      return password;
+      } 
+      else if (lowercase === false && uppercase == false && numbers === true && specialCharacters === false) {
+        for (i=1;i<passwordLength;i++) {
+          var c = Math.floor(Math.random()*chars6.length + 1);
+          password += chars6.charAt(c)
+        }
+        return password;
+        }
     }
-    return (userPassword)
   }
-}
-
-
 
 
 
