f9ab7d4d161bf8b49fa8383556cfc1d8c7c448d4
Emily Blair <emilyblair96@gmail.com>
2020-10-16 13:07:54 -0500

    Refractor generate password logic

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,14 @@
-// Assignment Code, locates the element in HTML
+// Assignment Code
 var generateBtn = document.querySelector("#generate");
-// Add event listener to generate button, adds this method to HTML element, calls the writePassword functio defined above
-generateBtn.addEventListener("click", writePassword);
 
-// var options = ["special", "alphabetUpper", "alphabetLower", "numbers"]
-// var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"]
-// var alphabetUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "Q", "R", "S", "T", "U", "V", "X", "Y", "Z"]
-// var alphabetLower = ["a", "b", "c"]
-// var numbers = ["0", "1", "2"]
+// Trying to figure out how to reset the password when user hits the generate button thing
+var resetBtn = document.createElement("button")
+var divButton = document.createElement("div")
+divButton.append(resetBtn)
+resetBtn.textContent = "Reset Password Generator"
+resetBtn.style.fontSize = "20px"
+var footer = document.querySelector(".card-footer")
+footer.append(divButton)
 
 // Write password to the #password input
 function writePassword() {
@@ -15,31 +16,60 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
+  chars = "";
 }
 
- function randomSelector () {
-  var selectCategory = options[Math.floor(Math.random() * 4)]
+// Reset the password generator
+function reset() {
+  var passwordText = document.querySelector("#password");
+  passwordText.value = ""
 
-    if (selectCategory === "special") {
-       return special[Math.floor(Math.random() * 5) + 1]
-    }
-
-    else if (selectCategory ==="alphabetUpper")  {
-      return alphabetUpper[Math.floor(Math.random() * 10) + 1]
-    }
-
-    else if (selectCategory ==="alphabetLower") {
-      return alphabetLower[Math.floor(Math.random() * 2) + 1]
-    }
-
-    else {
-      return numbers[Math.floor(Math.random() * 2) + 1]
-    }
-  
 }
 
-// Password prompts
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+// Add event listener to reset button
+resetBtn.addEventListener("click", reset)
+
+// Strings of the characters the user can choose to include their password
+var alphabetLower = "abcdefghijklmnopqrstuvwxyz"
+var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var num = "0123456789"
+var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-="
+var chars = "";
+
+// Add alphabetLower characters to the chars string if user presses OK
+function option1 (x) {
+  if (x===true) {
+    chars = chars.concat(alphabetLower)
+  }
+}
+
+// Add alphabetUpper characters to the chars string if user presses OK
+function option2 (x) {
+  if (x===true) {
+    chars = chars.concat(alphabetUpper)
+  }
+}
+
+// Add num characters to the chars string if user presses OK
+function option3 (x) {
+  if (x===true) {
+    chars = chars.concat(num)
+  }
+}
+
+// Add special characters to the chars string if user presses OK
+function option4 (x) {
+  if (x===true) {
+    chars = chars.concat(special)
+  }
+}
+
+// Generate a random password based on the users input
 function generatePassword() {
+
   var passwordLength = prompt("How long would you like your password to be?  Please enter a number between 8 - 128.");
 
   if (passwordLength < 8 || passwordLength > 128) {
@@ -49,69 +79,28 @@ function generatePassword() {
   else if (passwordLength >= 8 || passwordLength <= 128) {
 
     var lowercase = confirm("Would you like your password to include lowercase characters?");
-
+    option1(lowercase)
+    console.log(chars)
+    
     var uppercase = confirm("Would you like your password to include uppercase characters?");
+    option2(uppercase)
+    console.log(chars)
 
     var numbers = confirm("Would you like your password to include numbers?");
+    option3(numbers)
+    console.log(chars)
 
     var specialCharacters = confirm("Would you like your password to include special characters?")
+    option4(specialCharacters)
+    console.log(chars)
 
-    var password = "";
-    var special = "!@#$%^&*()_+~`|}{[]\:;?><,./-="
-    var alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    var alphabetLower = "abcdefghijklmnopqrstuvwxyz"
-    var num = "0123456789"
-    var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]\:;?><,./-=";
-    var chars2 = special.concat(alphabetUpper, num)
-    var chars3 =special.concat(alphabetLower, num)
-    var chars4 = special.concat(alphabetUpper, alphabetLower)
-    var chars5 = alphabetUpper.concat(alphabetLower, num)
-    var char6 = num.concat(special)
  
-    if (lowercase === true && uppercase == true && numbers === true && specialCharacters === true) {
-      for (i=1;i<passwordLength;i++) {
+      for (i=1; i<=passwordLength; i++) {
         var c = Math.floor(Math.random()*chars.length + 1);
         password += chars.charAt(c)
       }
       return password;
       }
-    else if (lowercase === false && uppercase == true && numbers === true && specialCharacters === true) {
-      for (i=1;i<passwordLength;i++) {
-        var c = Math.floor(Math.random()*chars2.length + 1);
-        password += chars2.charAt(c)
-      }
-      return password;
-      }
-    else if (lowercase === true && uppercase == false && numbers === true && specialCharacters === true) {
-        for (i=1;i<passwordLength;i++) {
-          var c = Math.floor(Math.random()*chars3.length + 1);
-          password += chars3.charAt(c)
-        }
-        return password;
-        }
-    else if (lowercase === true && uppercase == true && numbers === false && specialCharacters === true) {
-      for (i=1;i<passwordLength;i++) {
-        var c = Math.floor(Math.random()*chars4.length + 1);
-        password += chars4.charAt(c)
-      }
-      return password;
-      }
-    else if (lowercase === true && uppercase == true && numbers === true && specialCharacters === false) {
-      for (i=1;i<passwordLength;i++) {
-        var c = Math.floor(Math.random()*chars5.length + 1);
-        password += chars5.charAt(c)
-      }
-      return password;
-      } 
-      else if (lowercase === false && uppercase == false && numbers === true && specialCharacters === false) {
-        for (i=1;i<passwordLength;i++) {
-          var c = Math.floor(Math.random()*chars6.length + 1);
-          password += chars6.charAt(c)
-        }
-        return password;
-        }
+
     }
-  }
-
-
 
